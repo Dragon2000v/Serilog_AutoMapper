545bfdc6072029eb0419ca46299cb08765b81052
Vitaly <shmatko064@gmail.com>
2023-08-08 23:13:09 +0300

    Добавьте файлы проекта.

---
+++ MapperTestAutoMapper/AutoMapperProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+
+namespace MapperTestAutoMapper
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile() 
+        {
+            CreateMap<SuperHero, SuperHeroDto>();
+            CreateMap<SuperHeroDto,SuperHero>();
+        }
+    }
+}
---
+++ MapperTestAutoMapper/Controllers/SuperHeroController.cs
@@ -0,0 +1,55 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MapperTestAutoMapper.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        private static List<SuperHero> heroes = new List<SuperHero>
+        {
+            new SuperHero{ 
+                Id = 1,
+                Name="Spider Man",
+                FirstName="Peter",
+                LastName="Parker",
+                Place="New York City",
+                DataAdded = new DateTime(2001,08,10),
+                DataModified = null
+            },
+             new SuperHero{
+                Id = 2,
+                Name="Iron Man",
+                FirstName="Tony",
+                LastName="Stark",
+                Place="Malibu",
+                DataAdded = new DateTime(1970,05,29),
+                DataModified = null
+             }
+        };
+
+        private readonly IMapper _mapper;
+
+        public SuperHeroController(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public ActionResult<List<SuperHero>> GetHeroes() 
+        {     
+            
+            return Ok(heroes.Select(hero => _mapper.Map<SuperHeroDto>(hero))); 
+        }
+
+        [HttpPost]
+        public ActionResult<List<SuperHero>> AddHero(SuperHeroDto newHero)
+        {
+            var hero = _mapper.Map<SuperHero>(newHero);
+            heroes.Add(hero);
+            return Ok(heroes.Select(hero => _mapper.Map<SuperHeroDto>(hero)));
+        }
+    }
+}
